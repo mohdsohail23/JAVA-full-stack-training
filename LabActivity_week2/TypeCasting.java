c64863d7ffc2c1c5e7cb3870b5b294b41f27df9b
mohdsohail23 <86572018+mohdsohail23@users.noreply.github.com>
2021-07-25 13:54:29 +0530

    Update TypeCasting.java

--- LabActivity_week2/TypeCasting.java
+++ LabActivity_week2/TypeCasting.java
@@ -2,40 +2,32 @@ package LabActivity_week2;
 
 public class TypeCasting {
 	public static void main(String[] args) {
-		//compatible types
+		//valid identifiers
+		int age=20;//= is assignment operator
+		int _age;
+		int $age;
+		int _2a$e;
+		int a2_$e;
 		
-		byte b=127;
-		short s=b;
-		int i=s;
-		long l=i;
+//		//invalid identifiers
+//		int 2age;//you cannot start the identifier with a number
+//		int @age;//you cannot start the identifier with a special character other than $ and _
+//		int a g e;//you cannot have space between the characters
+//		int package;//you cannot have a keyword as a variable name
 		
-		l=257;//re initialize the long variable
-		System.out.println("long value..:"+l);
-		i=(int)l;//converting the 64 bit to 32
-		System.out.println("int value..:"+i);
-		s=(short)i;
-		System.out.println("short value..:"+s);
-		b=(byte)s;
-		System.out.println("byte value..:"+b);	
+		//initialization
+		long l=233323;//64 bit
+		int i=3434;//32 bit
+		short s=32767;//size is 16 bit and range is -32768 to 32767
+		byte b=127;//size is 8 bit and range is -128 to 127
 		
-		//auto promotion
-		byte mybite1=120;
-		byte mybite2=2;
-		byte result=(byte)(mybite1*mybite2);//when two bytes are multiplied then the result will be int (auto promotion)
-		System.out.println(result);
-		int myint=mybite1*mybite2;
-		System.out.println(myint);
+		double d=234.56;//64 bit
+		float myfloat=344.455f;//32 bit
 		
-		double d=23.45;//64 bit
-		float myf=(float)d;//32 bit
-		System.out.println(myf);
+		char c='h';
 		
-		char ch='a';//number type
-		byte myfloat=(byte)ch;
-		System.out.println(myfloat);
+		boolean boo=true;
+		boolean boo2=false;
 		
-		short myshortch=64;
-		char ccc=(char)myshortch;
-		System.out.println(ccc);
 	}
 }
