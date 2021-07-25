483b8b0e6adafd8196dc6d500b2ce9c833a79c4a
mohdsohail23 <86572018+mohdsohail23@users.noreply.github.com>
2021-07-25 14:07:03 +0530

    Delete LabActivity_week2 directory

--- LabActivity_week2/CommandLineArgs.java
+++
@@ -1,6 +0,0 @@
-public class CommandLineArgs {
-	public static void main(String[] args) {
-		System.out.println("this program accepts 2 command line arguments");
-	     System.out.println("KNIGHTINGALE--------"+args[0]+"-----------"+args[1]);
-	}	     
-}
--- LabActivity_week2/HelloConsole.java
+++
@@ -1,7 +0,0 @@
-
-public class HelloConsole{
-public static void main(String args[]){
-System.out.println("hello console!!!!");
-
-}
-}
